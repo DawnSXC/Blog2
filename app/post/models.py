56400f08a0de46dd0d646acd917f2b2609d7766c
DawnSXC <79969886+DawnSXC@users.noreply.github.com>
2021-12-05 02:13:48 +0800

    Add files via upload

--- app/post/models.py
+++ app/post/models.py
@@ -12,7 +12,7 @@ class Post(db.Model):
     like_times = db.Column(db.Integer, default=0)
     dislike_times = db.Column(db.Integer, default=0)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    category_id = db.Column(db.Integer,  nullable=False,default=1)
     post_img = db.Column(db.String(1000))
     post_comments = db.relationship('Comment', backref='post')
     collected = db.relationship('Collect', backref='post')
@@ -21,7 +21,7 @@ class Post(db.Model):
 class Comment(db.Model):
     __tablename__ = "comment"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    comment = db.Column(db.String(1000), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
     comment_time = db.Column(db.DateTime, default=datetime.now)
@@ -36,9 +36,3 @@ class Collect(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
 
-
-class Category(db.Model):
-    __tablename__ = 'category'
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    category_name = db.Column(db.String(30), nullable=False)
-    post = db.relationship('Post', backref='category')
